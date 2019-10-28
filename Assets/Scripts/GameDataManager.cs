95aea0ea5059dec51407b22bda78a606dbfd9981
nonzeronucleus <gitkraken@ianplumb.org>
2019-10-28 07:58:49 +0000

    Removes old game data

--- Assets/Scripts/GameDataManager.cs
+++ Assets/Scripts/GameDataManager.cs
@@ -15,33 +15,21 @@ public class GameDataManager
 
     static GameDataManager self;
 
-    public List<GameData> data=new List<GameData>();
     Reducer state = new BaseState();
     Selectors _selectors;
 
-    EnemyPositions enemyPositions = new EnemyPositions();
     int _currentTick = 0;
 
     public GameDataManager() {
     }
 
     public void init() {
-        data.Add(enemyPositions);
-
         state.init();
         _selectors = new Selectors(state);
 
         Action a = new AddTimedAction(new MoveEnemyThunk(), 5);
 
         handleAction(a);
-
-        data.ForEach(item => {
-            item.Start();
-        });
-    }
-
-    public EnemyPositions getEnemyPositions () {
-        return enemyPositions;
     }
 
     public void handleAction(Action action){
@@ -58,11 +46,6 @@ public class GameDataManager
         Action a = new TickAction(_currentTick);
 
         handleAction(a);
-
-    //     // Debug.Log(tick);
-        data.ForEach(item => {
-            item.Tick();
-        });
     }
 
     public Selectors GetSelectors() {
