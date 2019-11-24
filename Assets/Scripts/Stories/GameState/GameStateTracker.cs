f423de320fff570ca1ec4ce22d649a8b74bbb078
nonzeronucleus <gitkraken@ianplumb.org>
2019-11-24 20:33:41 +0000

    Adds text mesh and game status text

---
+++ Assets/Scripts/Stories/GameState/GameStateTracker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStories;
+using UnityEngine.UI;
+
+public class GameStateTracker : MonoBehaviour
+{
+    public StoriesHelper storiesHelper;
+    Text textField;
+
+    void Start()
+    {
+        textField = GetComponent<Text>();
+        Debug.Log(textField);
+        storiesHelper.Setup(gameObject, MapStoriesToProps);
+    }
+
+	void MapStoriesToProps(Story story)
+    {
+        GameStateStory gameStateStory = story.Get<GameStateStory>();
+
+        switch(gameStateStory.gameState){
+            case GameState.LOST:
+                textField.text = "Game Lost";
+                break;
+            case GameState.WON:
+                textField.text = "Game Won";
+                break;
+            default:
+                textField.text = "";
+                break;
+        }
+    }
+}
